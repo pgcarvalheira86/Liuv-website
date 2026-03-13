d57e4569788d9b0cb7609a247b5f92f4feb1501a
PUNEET KUMAR <puneet@liuv.io>
2026-03-14 02:46:02 +0530

    cookie update

--- netlify/functions/auth-logout.js
+++ netlify/functions/auth-logout.js
@@ -1,16 +1,18 @@
-import { clearAuthCookie, jsonResponse, redirectResponse } from '../../lib/auth-utils.js';
+import { clearAuthCookie, getCookieDomain, jsonResponse, redirectResponse } from '../../lib/auth-utils.js';
 
 export async function handler(event) {
-  // Support both GET (redirect) and POST (API)
+  const host = event.headers?.host || event.headers?.Host;
+  const domain = getCookieDomain(host);
+  const clearOpts = domain ? { domain } : {};
   if (event.httpMethod === 'GET') {
     return redirectResponse('/', {
-      'Set-Cookie': clearAuthCookie(),
+      'Set-Cookie': clearAuthCookie(clearOpts),
     });
   }
 
   return jsonResponse(
     { success: true, message: 'Logged out' },
     200,
-    { 'Set-Cookie': clearAuthCookie() }
+    { 'Set-Cookie': clearAuthCookie(clearOpts) }
   );
 }
